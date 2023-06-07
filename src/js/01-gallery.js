96cb8fd133e205ac67be064579319b10ef712054
Marisha9182 <120337365+Marisha9182@users.noreply.github.com>
2023-06-07 15:59:30 +0200

    task 1-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMarkupContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryMarkupContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+    return items
+    .map(({ original, preview, description }) => {
+        return `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>
+                </li>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const saveKey = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(saveKey)) || {};
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+// reloadPage();
+
+function storageFormData(e) {
+    formData[e.target.name] = e.target.value.trim();
+    localStorage.setItem(saveKey, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const { email, message } = e.currentTarget.elements;
+    console.log({ email: email.value, message: message.value });
+}
+
+function reloadPage() {
+    if (formData) {
+    let { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+    }
+}
